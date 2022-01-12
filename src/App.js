0220cc1c2908f1dfbaa88f3a3e092d587b9c8dd1
Oleg Ovramets <olegovramets@gmail.com>
2022-01-12 19:19:20 +0200

    update 1 homework

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React from "react";
-import Profile from "./components/Task_1/Profile/Profile"
-import Statistics from "./components/Task_2/Statistics/Statistics";
-import FriendList from "./components/Task_3/FriendList/FriendList";
-import TransactionHisory from "./components/Task_4/TransactionHistory/TransactionHistory";
-import user from "./user.json"
-import data from "./data.json"
-import friends from "./friends.json"
-import transactions from "./transactions.json"
-
-
+import React from 'react';
+import Profile from './components/Task_1/Profile/Profile';
+import Statistics from './components/Task_2/Statistics/Statistics';
+import FriendList from './components/Task_3/FriendList/FriendList';
+import TransactionHisory from './components/Task_4/TransactionHistory/TransactionHistory';
+import user from './user.json';
+import data from './data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 function App() {
   return (
-    <div>
-      <Profile 
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-      <Statistics 
-        data={data}
-        title="UPLOAD STATS"
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
-    <FriendList friends={friends} />
+      <Statistics data={data} title="UPLOAD STATS" />
+      <FriendList friends={friends} />
 
-    <TransactionHisory items={transactions} />
-    </div>
+      <TransactionHisory items={transactions} />
+    </>
   );
 }
 
--- src/components/Task_4/TransactionHistory/TransactionHistory.jsx
+++ src/components/Task_4/TransactionHistory/TransactionHistory.jsx
@@ -1,44 +1,42 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import styles from "./TransactionHistory.module.css"
+import styles from "./TransactionHistory.module.css";
 
 const TransactionHisory = ({ items }) => {
-    return(
+  return (
     <table className={styles.transactionHistory}>
-        <thead>
-            <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((transaction) => {
+          return (
+            <tr key={transaction.id}>
+              <td>{transaction.type}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.currency}</td>
             </tr>
-        </thead>
-        
-        <tbody>
-            {items.map(transaction => {
-                return (
-                    <tr key={transaction.id}>
-                        <td>{transaction.type}</td>
-                        <td>{transaction.amount}</td>
-                        <td>{transaction.currency}</td>
-                    </tr>
-                )
-            })}
-    
-        </tbody>
+          );
+        })}
+      </tbody>
     </table>
-    );
+  );
 };
 
 TransactionHisory.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.string.isRequired,
-            currency: PropTypes.string.isRequired,
-        })
-    )
-    
-}
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
 
 export default TransactionHisory;
